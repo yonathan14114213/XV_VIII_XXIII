1d1597ff90cfa71a751a0918f1fdc7cc763a9dd2
yonathan <yonathan.eisenberg@gmail.com>
2023-08-16 16:45:20 +0300

    8

--- 2/app.js
+++ 2/app.js
@@ -1,33 +1,84 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+const bcrypt = require('bcrypt');
+const saltRounds = 5;
+
+const myUser = {
+    name: 'menahem eisenbreg',
+    id: uuidv4(),
+    Email: "menahem@example.com",
+    password: ""
+};
+
+ async function enigma(password, i) {return await bcrypt.hash(password.toString(), 10)}
+.then(hashed => {
+    a.password = hashed;
+    return 
+})
+.then(console.log);
+}
 
 const users = [{
-    'name': 'menahem eisenbeg',
-    'id': 0,
-    'e-mail': "menahem@example.com"
+    'name': 'menahem eisenbreg',
+    'id': uuidv4(),
+    Email: "menahem@example.com",
+    password: enigma('jhg', 0).then(hash => users[0].password = hash)
 },{
     name: 'yael eisenberg',
-    id: 1,
-    'e-mail': 'yael@example.com'
+    id: uuidv4(),
+    Email: 'yael@example.com',
+    password:""
 },{
     name: 'noa eisenberg',
-    id: 2,
-    'e-mail': 'noa@example.com'
+    id: uuidv4(),
+    Email: 'noa@example.com',
+    password: enigma('hogla', users, 2)
 },{
     name: 'yitzik eisenberg',
-    id: 3,
-    'e-mail': 'yitzik@example.com'
+    id: uuidv4(),
+    Email: 'yitzik@example.com',
+    password:enigma("goat", users, 3)
 },{
     name: 'yoni eisenberg',
-    id: 4,
-    'e-mail': 'yoni@example.com'
+    id: uuidv4(),
+    Email: 'yoni@example.com',
+    password:("yonathan", users, 4)
 }]
+.then(
+    app.get('/', (req, res)=>{
+        res.send(users[0].password)
+    }));
 
 app.get('/user/:id', (req, res)=>{
-    const id = req.params.id
-    res.send(users[id])
- })
+    res.send(users[req.params.id]);
+ });
+
+app.post('/', (req, res)=>{
+    users.push({
+        name: req.body.name,
+        id: uuidv4(),
+        Email: req.body.Email,
+        password: enigma(req.body.password, users, saltRounds)
+    });
+    res.send(users);
+    saltRounds+=1;
+});
+
+app.put('/user/:id',(req, res)=>{
+    console.log('req came!');
+    users[req.params.id].name = req.body.name;
+    users[req.params.id].Email = req.body.Email;
+    res.send(users);
+});
+
+app.delete('/user/:id', (req, res)=>{
+    users.splice(req.params.id,1);
+    res.send(users);
+});
 
 // app.get('/user/:id', (req, res) => {
 //     const id = req.params.id
@@ -38,6 +89,18 @@ app.get('/user/:id', (req, res)=>{
 //     } 
 // });
 
-app.listen(port, () => {
+app.post('/login/:email/:password', (req, res)=>{
+    for (let i = 0;i<users.length;i++){
+        if (req.params.email === users[i].Email && req.params.password!== users[i].password){
+            res.send('the password is uncorrect');
+        }
+        if (req.params.email===users[i].Email && req.params.password===users[i].password) {
+            res.send('user connected')
+        }
+    }
+})
+
+app.listen(port, (req, res) => {
     console.log(`Server is up and running on port:${port}`);
+    //res.send('for identifeiding enter user_name and password');
 });
